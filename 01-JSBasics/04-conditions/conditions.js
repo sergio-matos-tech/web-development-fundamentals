1e2b405c5b1cf63c3dae631727932c2c3e50fbff
Sergio Henrique <sergimmatos@hotmail.com>
2025-06-20 17:56:49 -0300

    Conditions in JS

---
+++ 01-JSBasics/04-conditions/conditions.js
@@ -0,0 +1,22 @@
+
+console.log('a' < 'A');
+console.log('11' == 11);
+
+
+let random = Math.floor(Math.random() * 10)
+
+console.log(`Generated = ${random}`);
+if (random == 5) {
+    console.log(5);
+} else {
+    console.log("Not 5");
+}
+
+
+console.log(isEven(random));
+
+
+function isEven(num) {
+    return (num % 2 == 0 ? true : false)
+}
+
