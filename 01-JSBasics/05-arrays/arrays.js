fe62f8b11d9e2165fbed5a5cda424d4d114e29cc
Sergio Henrique <sergimmatos@hotmail.com>
2025-06-20 19:40:01 -0300

    Working with arrays in JS

---
+++ 01-JSBasics/05-arrays/arrays.js
@@ -0,0 +1,20 @@
+
+let lottoNumbers = []
+
+for (i = 0; i < 6; i++) {
+    lottoNumbers[i] = Math.floor(Math.random() * 10)
+}
+
+console.log(lottoNumbers);
+
+
+const leaderboard = ['Harry', 'Lua', 'Hermione', 'Bellatrix']
+
+
+leaderboard[1] = 'Luna'
+leaderboard[leaderboard.length - 1] = 'Draco'
+leaderboard[leaderboard.length] = 'Cuda'
+leaderboard.push('Lua')
+leaderboard.splice(1, 1)
+
+console.log(leaderboard);
---
+++ 01-JSBasics/05-arrays/arrays2.js
@@ -0,0 +1,16 @@
+
+
+const planets = ['The Moon','Venus', 'Earth', 'Mars', 'Jupiter']
+
+console.log(planets)
+
+// removing the first element of the array
+planets.shift()
+
+// adding 'Saturn' at the end of the array
+planets.push('Saturn')
+
+// adding 'Mercury' as the first element of the array
+planets.unshift('Mercury')
+
+console.log(planets)
