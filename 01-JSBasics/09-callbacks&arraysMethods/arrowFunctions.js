d5dc9526079f6c7e705d232dfa0026bf630d8be7
Sergio Henrique <sergimmatos@hotmail.com>
2025-07-23 19:19:14 -0300

    Working with array methods and callbacks

---
+++ 01-JSBasics/09-callbacks&arraysMethods/arrowFunctions.js
@@ -0,0 +1,15 @@
+
+
+const add = (x, y) => {
+    return x + y
+}
+
+console.log(add(32, 32));
+console.log("x", "y");
+
+const greet = (string) => {
+    return `Hey ${string}!`
+}
+
+console.log(greet("Hagrid"))
+console.log(greet("Luna"))
---
+++ 01-JSBasics/09-callbacks&arraysMethods/test1.js
@@ -0,0 +1,15 @@
+
+
+const numbers = [32, 20, 453, 83, 4, 3, 98, 12, 23, 3]
+
+numbers.forEach(number => {
+    console.log(number);
+});
+
+console.log("\nEven numbers: ");
+numbers.forEach(number => number % 2 == 0 ? console.log(number): "");
+
+
+const doubleNumbers = numbers.map(x => x * 2)
+
+console.log(doubleNumbers);
