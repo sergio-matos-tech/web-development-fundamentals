a049a90d4db2663829529b41ead503071565bc71
Sergio Henrique <sergimmatos@hotmail.com>
2025-07-28 19:47:25 -0300

    Finishing up with functions

---
+++ 01-JSBasics/09-callbacks&arraysMethods/defaultParam.js
@@ -0,0 +1,18 @@
+
+function rollDie(numSides = 6) {
+
+    return Math.floor(Math.random() * numSides) + 1
+}
+
+console.log(rollDie(20))
+console.log(rollDie())
+
+
+function greet(message = "World") {
+    return `Hello ${message}`
+}
+
+
+console.log(greet("Markus"));
+console.log(greet());
+
