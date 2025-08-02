60ce51def5e73a7705d0bf28c9a1b8336ff144a2
Sergio Henrique <sergimmatos@hotmail.com>
2025-08-01 22:39:47 -0300

    Working with DOM part II

---
+++ 01-JSBasics/10-DOM/challenge02/app.js
@@ -0,0 +1,6 @@
+
+let doneTodos = document.querySelectorAll('.done')
+
+let checkbox = document.querySelector('#scales')
+
+
--- 01-JSBasics/10-DOM/test01/app.js
+++ 01-JSBasics/10-DOM/test01/app.js
@@ -2,3 +2,11 @@
 
 const bannerId = document.getElementById('toc')
 
+const allImages = document.getElementsByTagName('img')
+
+for (let img of allImages) {
+    console.log(img.src)
+}
+
+const squareClass = document.getElementsByClassName('square')
+
