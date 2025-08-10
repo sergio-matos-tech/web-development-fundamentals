cc1660a8824be6ca5885b2842b684d7b828e4745
Sergio Henrique <sergimmatos@hotmail.com>
2025-08-10 14:17:13 -0300

    Working with event listerners

---
+++ 01-JSBasics/10-DOM/challenge07/app.js
@@ -0,0 +1,12 @@
+
+const helloButton = document.getElementById('hello')
+
+helloButton.addEventListener('click', () => {
+    console.log('hello')
+})
+
+const goodbyeButton = document.getElementById('goodbye')
+
+goodbyeButton.addEventListener('click', () => {
+    console.log('goodbye')
+})
---
+++ 01-JSBasics/10-DOM/events_tests/app.js
@@ -0,0 +1,19 @@
+
+const button = document.querySelector('button')
+
+button.addEventListener('click', () => {
+    alert('You clicked the button! ')
+})
+
+
+button.onmouseenter = function() {
+    console.log('Mouse over')
+}
+
+
+const buttonv2 = document.getElementById('bttn-v2')
+
+buttonv2.addEventListener('click', () => {
+    alert('You clicked the v2 button!')
+})
+
