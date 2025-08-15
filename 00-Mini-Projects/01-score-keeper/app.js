c84a54d806d8c97d63a6a557f62491216f6ee0dc
Sergio Henrique <sergimmatos@hotmail.com>
2025-08-15 14:18:32 -0300

    mini project Score Keeper tested and finished

--- 00-Mini-Projects/01-score-keeper/app.js
+++ 00-Mini-Projects/01-score-keeper/app.js
@@ -5,6 +5,7 @@ const resetButton = document.getElementById('reset');
 
 const playerOneScoreDisplay = document.getElementById('player-one-score');
 const playerTwoScoreDisplay = document.getElementById('player-two-score');
+const winingScoreSelect = document.getElementById('playto');
 
 let p1Score = 0;
 let p2Score = 0;
@@ -15,7 +16,11 @@ playerOneButton.addEventListener('click', function() {
     
     if (!gameOver) {
         p1Score++;
-        if (p1Score === winingScore) gameOver = true;
+        if (p1Score === winingScore) {
+            gameOver = true;
+            playerOneScoreDisplay.classList.add('winner');
+            playerTwoScoreDisplay.classList.add('loser');
+        } 
         playerOneScoreDisplay.textContent = p1Score;
     } 
 });
@@ -23,7 +28,33 @@ playerOneButton.addEventListener('click', function() {
 playerTwoButton.addEventListener('click', function() {
     if (!gameOver) {
         p2Score++;
-        if (p2Score === winingScore) gameOver = true;
+        if (p2Score === winingScore) {
+            gameOver = true;
+            playerTwoScoreDisplay.classList.add('winner');
+            playerOneScoreDisplay.classList.add('loser');
+            
+        }
         playerTwoScoreDisplay.textContent = p2Score;
-    } 
+    }
 });
+
+resetButton.addEventListener('click', function() {
+    reset();
+});
+
+function reset() {
+    p1Score = 0;
+    p2Score = 0;
+    gameOver = false;
+    playerOneScoreDisplay.textContent = p1Score;
+    playerTwoScoreDisplay.textContent = p2Score;
+    playerOneScoreDisplay.classList.remove('winner', 'loser');
+    playerTwoScoreDisplay.classList.remove('winner', 'loser');
+    winingScoreSelect.value = winingScore; 
+}
+
+winingScoreSelect.addEventListener('change', function() {
+    alert('Wining score changed to ' + this.value);
+    winingScore = parseInt(this.value);
+    reset();
+})
